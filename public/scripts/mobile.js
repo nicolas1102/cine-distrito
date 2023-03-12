3497f24a27fee3c1500d8f90dc4264b2ab0c912e
adrianadiazv <nicolasdiazv1102@gmail.com>
2023-03-12 17:25:49 -0500

    navigation bar

---
+++ public/scripts/mobile.js
@@ -0,0 +1,20 @@
+const mobileMenuBtnElement = document.getElementById('mobile-menu-btn');
+const mobileMenuElement = document.getElementById('mobile-menu');
+
+function toggleMobileMenu () {
+    mobileMenuElement.classList.toggle('open');
+}
+
+mobileMenuBtnElement.addEventListener('click', toggleMobileMenu);
+
+
+
+
+
+
+
+
+
+
+
+
