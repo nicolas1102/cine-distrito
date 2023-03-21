549506469dd99d3ad2fcb17b28f08f6deb475e51
adrianadiazv <nicolasdiazv1102@gmail.com>
2023-03-20 21:24:29 -0500

    user cart part 2

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,6 +1,26 @@
-function addCartItem(req, res) {
+const Product = require('../models/product.model');
+
+
+async function addCartItem(req, res, next) {
+    let product;
+    try {
+        product = await Product.findById(req.body.productId);
+    } catch (error) {
+        next(error);
+        return;
+    }
+    // we save the item in the cart 
+    const cart = res.locals.cart;
     //  we access to the cart saved in the locals; we created it in the cart middleware
-    res.locals.cart.addItem();
+    cart.addItem(product);
+
+    // and we update the session cart; this cart survive this request response cycle
+    req.session.cart = cart;
+
+    res.status(201).json({
+        message: 'Cart updated',
+        newTotalItems: cart.totalQuantity,
+    });
 }
 
 module.exports = {
--- models/cart.model.js
+++ models/cart.model.js
@@ -1,7 +1,9 @@
 class Cart {
     // default values
-    constructor(items = []) {
+    constructor(items = [], totalQuantity = 0, totalPrice = 0) {
         this.items = items;
+        this.totalQuantity = totalQuantity;
+        this.totalPrice = totalPrice;
     }
 
     addItem(product) {
@@ -14,14 +16,23 @@ class Cart {
         for (let i = 0; i< this.items.length; i++){
             const item = this.items[i];
             if (item.product.id === product.id) {
+                //  we update the cart product data
                 cartItem.quantity = cartItem.quantity + 1;
                 cartItem.totalPrice = cartItem.totalPrice + product.price;
                 this.items[i] = cartItem;
+
+                //  we update the cart data
+                this.totalQuantity = this.totalQuantity + 1;
+                this.totalPrice = this.totalPrice + product.price;
                 return;
             }
         }
         // if the item is nnot alredy in the cart, we added
         this.items.push(cartItem);
+
+        //  we update the cart data
+        this.totalQuantity = this.totalQuantity + 1;
+        this.totalPrice = this.totalPrice + product.price;
     }
 }
 
